03b0fa39ec4d486abaf1d7bce731f4730e3d1f8c
Satendra Yadav <satendra@fisdom.com>
2025-03-03 13:16:07 +0530

    server url updated

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5000';
+const API_BASE_URL = 'https://time-series-analysis-full-stack-app.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
